8adefd7b7b3e7e6531bb62f0d6c6ddb7a1436b9e
You safe <youssef.smimaa@gmail.com>
2017-08-20 20:00:41 +0100

    Kick off at 20-08-2017

---
+++ Business/src/main/java/org/revision/spring_hibernate_angularJs/App.java
@@ -0,0 +1,13 @@
+package org.revision.spring_hibernate_angularJs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
